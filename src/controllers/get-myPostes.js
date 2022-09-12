589bd6d1a7df77128a452fba626fa102ae924606
lithhalim <103363189+lithhalim@users.noreply.github.com>
2022-09-12 13:09:00 +0300

    lith

--- src/controllers/get-myPostes.js
+++ src/controllers/get-myPostes.js
@@ -1,10 +1,10 @@
 const  postes_Model=require("../model/postes");
 
 module.exports=async(req,res)=>{
-    const catagory=req.params.data
+    const catagory=req.params.data;
 
     const DataUse=await postes_Model.findAll({
-        where:{catagory:catagory}
+        where:{catagories:catagory}
     })
     res.json(DataUse);
 }
